0e5822c7acc7778c41e229c24141acc3c5459242
sushmasrinivasan04@gmail.com <sushmasrinivasan04@gmail.com>
2023-09-15 09:42:42 +0200

    Add project files.

---
+++ module6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace module6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Sushma";
+            string lastName = "Srinivasan";
+
+            string fullName=firstName + " " + lastName; 
+
+            string Name=firstName.ToLower()+" " + lastName.Trim().ToLower();
+
+Console.WriteLine(fullName);
+            Console.WriteLine(Name);
+
+            int length=Name.Length;
+
+            if(firstName.Contains("sush")||firstName.Contains("Sush"))
+            {
+                Console.WriteLine("It's Sushma");
+            }
+            string substring = fullName.Substring(2, 4);
+            Console.WriteLine(substring);
+
+
+            Utilities.escapeCharacters();
+
+        }
+    }
+}
---
+++ module6/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module6
+{
+    internal class Utilities
+    {
+        public static void escapeCharacters()
+        {
+            string fName = "Smaran";
+            string lName = "Srini";
+
+            string displayName = $"Welcome!\n{fName}\t{lName}";
+            Console.WriteLine(displayName);
+            Console.ReadLine();
+        }
+
+    }
+}
